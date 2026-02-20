760b0e8f74c9cf8f3e8591e5c8e506c5469fbaa8
Daniel Alvarez <coda.alvarez@gmail.com>
2026-02-19 22:24:56 -0600

    Add calculateDashboard in commission-engine service

--- src/app/domain/comission-engine/commission-engine.service.ts
+++ src/app/domain/comission-engine/commission-engine.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { CommissionConfig } from 'src/app/store/commission-config/commission-config.model';
+// Models
+import { Contract } from 'src/app/store/contract/contract.model';
+import { Deposit } from 'src/app/store/deposit/deposit.model';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class CommissionEngineService {
-  
+
+	public calculateDashboard(
+		contracts: Contract[],
+		deposits: Deposit[],
+		commissionConfigs: CommissionConfig[],
+		from: number,
+		to: number
+	) {
+
+	}
 }
